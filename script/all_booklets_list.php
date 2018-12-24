cf9c7f083c894ba901d12d8e0350cfef8e6d2bc1
NogaBar <27781426+NogaBar@users.noreply.github.com>
2018-12-24 12:35:36 +0200

    Add file for all booklets - input to the script

---
+++ script/all_booklets_list.php
@@ -0,0 +1,50 @@
+<?php
+//images are found in index\image folder
+//booklets are the English directory of the epub file
+//booklet description is the hebrew name of the booklet
+$booklets = array();
+$images = array();
+$booklet_description = array();
+
+$booklets[0] = "bankrupcy";
+$images[0] = "bankrupcy_logo.jpg";
+$booklet_description[0] = "על בעיות פשיטת רגל בתורת המשחקים";
+
+$booklets[1] = "companies";
+$images[1] = "companies_logo.jpg";
+$booklet_description[1] = "מבוא לדיני חברות";
+
+$booklets[2] = "decision_making";
+$images[2] = "decision_making_logo.jpg";
+$booklet_description[2] = "קבלת החלטות ושימוש בידע";
+
+$booklets[3] = "evolution";
+$images[3] = "evolution_logo.jpg";
+$booklet_description[3] = "אבולוציה";
+
+$booklets[4] = "intro_to_logic";
+$images[4] = "logic_logo.jpg";
+$booklet_description[4] = "מבוא ללוגיקה מתמטית";
+
+$booklets[5] = "law_philosophy";
+$images[5] = "law_philosophy_logo.jpg";
+$booklet_description[5] = "מבוא לפילוסופיה של המשפט";
+
+$booklets[6] = "law-private_and_public";
+$images[6] = "law_public_and_private_logo.jpg";
+$booklet_description[6] = "משפט ציבורי ומשפט פרטי";
+
+$booklets[7] = "shoresh";
+$images[7] = "sqrt_logo.jpg";
+$booklet_description[7] = "על השורש הריבועי של שתיים ושורשים אחרים";
+
+$booklets[8] = "time_to_think";
+$images[8] = "time_to_think_logo.jpg";
+$booklet_description[8] = "זמן למחשבה: על משמעות הזמן בחיינו";
+
+$booklets[9] = "israel_borders";
+$images[9] = "israel_borders.png";
+$booklet_description[9] = "גבולות ארץ ישראל ומדינת ישראל";
+
+
+?>
